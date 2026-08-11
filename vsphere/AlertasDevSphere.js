8a30701624bfb7ff7d92e5c54c3a4c2f5fec9dac
ilucero-wetcom <ian.lucero@wetcom.com>
2026-08-11 10:19:08 -0300

    fix(vsphere): restaurar prefijo 'AVS - ' en blobs para evitar que Drive los descarte por nombre duplicado

--- vsphere/AlertasDevSphere.js
+++ vsphere/AlertasDevSphere.js
@@ -89,6 +89,19 @@ class VsphereAlertsProcessor extends MailProcessor {
       config.isAVS = isAVS;
     }
     
+    // --- FIX AVS DRIVE: Renombrar los blobs en memoria para que no se pisen al subir a Drive ---
+    if (isAVS && this.extractedBlobs) {
+      this.extractedBlobs.forEach(blob => {
+        const currentName = blob.getName();
+        if (!currentName.toUpperCase().includes('AVS -')) {
+          blob.setName("AVS - " + currentName);
+        }
+      });
+      if (attachment && !attachment.getName().toUpperCase().includes('AVS -')) {
+        attachment.setName("AVS - " + attachment.getName());
+      }
+    }
+    
     return config;
   }
 
